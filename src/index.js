511546405f45942b2138fde29e9712cae80bbe88
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2021-06-03 17:04:37 -0500

    usando render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+//const element = document.createElement('h1')
+//element.innerText = "Hello Brenda"
 
-// const container = document.getElementById('app');
+//const container = document.getElementById('app')
+//container.appendChild(element)
 
-// container.appendChild(element);
+
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = <h1>Hello, Brenda </h1>
+const container = document.getElementById('app')
 
-const container = document.getElementById('app');
+//ReactDOM.render(__que__,__donde__)
+ReactDOM.render(element,container)
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+// Si usamos jsx en todos los archivos debemos importar react
