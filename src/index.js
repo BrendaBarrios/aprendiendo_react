f5105d5a0490794f3e881761aa95d93fec71543d
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2021-06-03 19:52:00 -0500

    ELementos jsx y paso expresiones javascript

--- src/index.js
+++ src/index.js
@@ -7,12 +7,24 @@
 
 
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Brenda </h1>
+// const jsx = <h1>Hello React</h1>
+// const element = React.createElement('a',{href:'https://platzi.com'},'Ir a platzi')
+// const container = document.getElementById('app')
+
+const name = 'Brenda'
+const element = React.createElement('h1',{},`Hola soy ${name}`)
 const container = document.getElementById('app')
+ReactDOM.render(element,container)
+
+
+// ELemento con jsx
+const suma =() => 3+3;
+const jsx = <h1>Soy otro elemento con jsx {suma()}</h1> 
+ReactDOM.render(jsx,container)
 
 //ReactDOM.render(__que__,__donde__)
-ReactDOM.render(element,container)
+
 
 // Si usamos jsx en todos los archivos debemos importar react
